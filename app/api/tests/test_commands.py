53470216b6d8fb7c800faa82861f36cd9ea99c81
hc-sun <34866479+hc-sun@users.noreply.github.com>
2023-11-01 18:43:55 +0800

    fixed linting error

--- app/api/tests/test_commands.py
+++ app/api/tests/test_commands.py
@@ -26,8 +26,10 @@ class CommandTests(SimpleTestCase):
         Test case for when the database is not immediately available.
         """
 
-        patched_check.side_effect = [Psycopg2OpError] * 2 + [OperationalError] * 2 + [True]
+        patched_check.side_effect = [Psycopg2OpError] * 2 \
+            + [OperationalError] * 2 + [True]
         call_command('wait_for_db')
-        # Assert that the check was called six times before a successful connection
+        # Assert that the check was called
+        # six times before a successful connection
         self.assertEqual(patched_check.call_count, 5)
         patched_check.assert_called_with(databases=['default'])
