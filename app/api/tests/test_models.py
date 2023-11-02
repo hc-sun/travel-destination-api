c00ef245cbdbfc231fde360bac67dac49b7520bb
hc-sun <34866479+hc-sun@users.noreply.github.com>
2023-11-02 18:18:37 +0800

    added custom user model

--- app/api/tests/test_models.py
+++ app/api/tests/test_models.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTest(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
         email = "test@example.com"
         password = "testpass123"
-        user = get_user_model().objects.create_user().objects.create_user(
+        user = get_user_model().objects.create_user(
             email=email,
             password=password
         )
