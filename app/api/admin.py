cc6ea0b820f0b8115495cb5d18fbc833edffe13a
hc-sun <34866479+hc-sun@users.noreply.github.com>
2023-11-04 17:45:21 +0800

    added custom fields in admin site

--- app/api/admin.py
+++ app/api/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from api import models
+from django.utils.translation import gettext as _
+
 
 class UserAdmin(BaseUserAdmin):
     '''
@@ -8,5 +10,16 @@ class UserAdmin(BaseUserAdmin):
     '''
     ordering = ["id"]
     list_display = ["email", "name"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (("Personal Info"), {"fields": ("name",)}),
+        (
+            _("Permissions"),
+            {"fields": ("is_active", "is_staff", "is_superuser")}
+        ),
+        (("Important dates"), {"fields": ("last_login",)}),
+    )
+    readonly_fields = ("last_login",)
+
 
 admin.site.register(models.User, UserAdmin)
