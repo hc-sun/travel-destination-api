5d82c32800331719ce508b7ff82fdc0213ee2e9f
hc-sun <34866479+hc-sun@users.noreply.github.com>
2023-11-04 16:28:29 +0800

    added django admin site

--- app/api/admin.py
+++ app/api/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin # noqa
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from api import models
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    '''
+    Custom user admin
+    '''
+    ordering = ["id"]
+    list_display = ["email", "name"]
+
+admin.site.register(models.User, UserAdmin)
