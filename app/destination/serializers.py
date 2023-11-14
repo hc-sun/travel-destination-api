c090167a3d9f92d43c027d5cb26cd1308507c353
hc-sun <34866479+hc-sun@users.noreply.github.com>
2023-11-14 21:12:54 +0800

    added TagSerializer in DestiantionSerializer

--- app/destination/serializers.py
+++ app/destination/serializers.py
@@ -2,8 +2,19 @@ from rest_framework import serializers
 from api.models import Destination, Tag
 
 
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tag objects"""
+
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
+
+
 class DestinationSerializer(serializers.ModelSerializer):
     """Serializer for destination objects"""
+
+    tags = TagSerializer(many=True, required=False)
 
     class Meta:
         model = Destination
@@ -13,20 +24,31 @@ class DestinationSerializer(serializers.ModelSerializer):
             'country',
             'city',
             'rating',
+            'tags',
         )
         read_only_fields = ('id',)
+
+    def create(self, validated_data):
+        """Create and return a new destination"""
+        # need to remove tags from validated_data first
+        # since it's not in the destination model
+        tags = validated_data.pop('tags', [])
+
+        destination = Destination.objects.create(**validated_data)
+        auth_user = self.context['request'].user
+
+        for tag in tags:
+            # get_or_create will create a new tag if it doesn't exist
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                name=tag['name']
+                )
+            destination.tags.add(tag_obj)
+
+        return destination
 
 
 class DestinationDetailSerializer(DestinationSerializer):
     """Serializer for destination detail objects"""
     class Meta(DestinationSerializer.Meta):
         fields = DestinationSerializer.Meta.fields + ('description',)
-
-
-class TagSerializer(serializers.ModelSerializer):
-    """Serializer for tag objects"""
-
-    class Meta:
-        model = Tag
-        fields = ('id', 'name')
-        read_only_fields = ('id',)
